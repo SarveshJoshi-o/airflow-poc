6c94cf043e7d12a48b1bf560834358933e5e56d1
sarveshj <sarvesh.j@outdu.com>
2025-06-19 17:36:55 +0530

    Cleaned up the code and removed unnecessary comments

--- dags/personalized_newsletter.py
+++ dags/personalized_newsletter.py
@@ -92,43 +92,6 @@ def personalized_newsletter():
     # _get_weather_info = get_weather_info(users=_get_user_info)
     _get_weather_info = get_weather_info.expand(user=_get_user_info)
 
-    # @task(
-    #     outlets=[Asset("personalized_newsletter")],
-    #     max_active_tis_per_dag=1,
-    # )
-    # def create_personalized_newsletter(user_info_plus_weather: list[dict], **context: dict) -> None:
-    #     from airflow.sdk import ObjectStoragePath
-    #
-    #     date = context["dag_run"].run_after.strftime("%Y-%m-%d")
-    #
-    #     for user in user_info_plus_weather:
-    #         id = user["id"]
-    #         name = user["name"]
-    #         location = user["location"]
-    #         actual_temp = user["weather"]["current"]["temperature_2m"]
-    #         apparent_temp = user["weather"]["current"]["apparent_temperature"]
-    #         relative_humidity = user["weather"]["current"]["relative_humidity_2m"]
-    #
-    #         new_greeting = (
-    #             f"Hi {name}! \n\nIf you venture outside right now in {location}, you'll find the temprature to be "
-    #             f"{actual_temp}°C, but it will feel more like {apparent_temp}°C. The relative humidity is "
-    #             f"{relative_humidity}%."
-    #         )
-    #
-    #         object_storage_path = ObjectStoragePath(
-    #             f"{OBJECT_STORAGE_SYSTEM}://{OBJECT_STORAGE_PATH_NEWSLETTER}",
-    #             conn_id=OBJECT_STORAGE_CONN_ID,
-    #         )
-    #
-    #         daily_newsletter_path = object_storage_path/f"{date}_newsletter.txt"
-    #         generic_content = daily_newsletter_path.read_text()
-    #
-    #         personalized_content = generic_content.replace("Hello Cosmic Traveller,", new_greeting)
-    #         personalized_newsletter_path = object_storage_path/f"{date}_newsletter_userid_{id}.txt"
-    #         personalized_newsletter_path.write_text(personalized_content)
-    #
-    # create_personalized_newsletter(user_info_plus_weather=_get_weather_info)
-
     @task(max_active_tis_per_dag=16)
     def create_personalized_quote(
             system_prompt,
@@ -248,8 +211,6 @@ def personalized_newsletter():
         daily_newsletter_path = object_storage_path / f"{date}_newsletter.txt"
 
         generic_content = daily_newsletter_path.read_text()
-
-        print(new_greeting)
 
         updated_content = generic_content.replace("Hello Cosmic Traveller,", new_greeting)
 
